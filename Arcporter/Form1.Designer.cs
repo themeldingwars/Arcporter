bdc646500b7ec214965d2330859ba71e712f5119
freakbyte <freakbyte@gmail.com>
2019-05-20 15:48:57 +0200

    Initial commit

---
+++ Arcporter/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Arcporter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.darkButton1 = new DarkUI.Controls.DarkButton();
+            this.darkTextBox1 = new DarkUI.Controls.DarkTextBox();
+            this.darkListView1 = new DarkUI.Controls.DarkListView();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // darkButton1
+            // 
+            this.darkButton1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.darkButton1.Location = new System.Drawing.Point(260, 461);
+            this.darkButton1.Name = "darkButton1";
+            this.darkButton1.Padding = new System.Windows.Forms.Padding(5);
+            this.darkButton1.Size = new System.Drawing.Size(30, 23);
+            this.darkButton1.TabIndex = 2;
+            this.darkButton1.Text = "...";
+            this.darkButton1.Click += new System.EventHandler(this.DarkButton1_Click);
+            // 
+            // darkTextBox1
+            // 
+            this.darkTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.darkTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(73)))), ((int)(((byte)(74)))));
+            this.darkTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.darkTextBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.darkTextBox1.Location = new System.Drawing.Point(-1, 461);
+            this.darkTextBox1.Name = "darkTextBox1";
+            this.darkTextBox1.ReadOnly = true;
+            this.darkTextBox1.Size = new System.Drawing.Size(259, 20);
+            this.darkTextBox1.TabIndex = 1;
+            this.darkTextBox1.Text = "Please locate FirefallClient.exe";
+            // 
+            // darkListView1
+            // 
+            this.darkListView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.darkListView1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(56)))), ((int)(((byte)(58)))), ((int)(((byte)(60)))));
+            this.darkListView1.Location = new System.Drawing.Point(0, 0);
+            this.darkListView1.Name = "darkListView1";
+            this.darkListView1.Size = new System.Drawing.Size(289, 459);
+            this.darkListView1.TabIndex = 0;
+            this.darkListView1.Text = "darkListView1";
+            this.darkListView1.DoubleClick += new System.EventHandler(this.DarkListView1_DoubleClick);
+            this.darkListView1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.DarkListView1_KeyUp);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "FirefallClient.exe";
+            this.openFileDialog1.Filter = "Game Client | FirefallClient.exe";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.OpenFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(289, 480);
+            this.Controls.Add(this.darkButton1);
+            this.Controls.Add(this.darkTextBox1);
+            this.Controls.Add(this.darkListView1);
+            this.Name = "Form1";
+            this.Text = "Arcporter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DarkUI.Controls.DarkListView darkListView1;
+        private DarkUI.Controls.DarkTextBox darkTextBox1;
+        private DarkUI.Controls.DarkButton darkButton1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Arcporter/Form1.cs
@@ -0,0 +1,124 @@
+﻿using FauFau.Formats;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Arcporter
+{
+    public partial class Form1 : Form
+    {
+
+        [DllImport("user32.dll")]
+        static extern IntPtr LoadIcon(IntPtr hInstance, IntPtr iconName);
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetModuleHandle(string moduleName);
+
+        string mapsPath;
+        string exePath;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // reuse exe icon for titlebar to avoid duplicate data
+            IntPtr hInstance = GetModuleHandle(null);
+            IntPtr hIcon = LoadIcon(hInstance, new IntPtr(32512));
+            if (hIcon != IntPtr.Zero)
+            {
+                Icon = Icon.FromHandle(hIcon);
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            TryLoad(Properties.Settings.Default.Path, true);
+        }
+
+        private void TryLoad(string path, bool ignoreError = false)
+        {
+            
+            if(File.Exists(path))
+            {
+                FileInfo fi = new FileInfo(path);
+                if(fi.Directory.Parent != null)
+                {
+                    mapsPath = Path.Combine(fi.Directory.Parent.FullName, "maps");
+                    if(Directory.Exists(mapsPath))
+                    {
+                        exePath = path;
+                        darkTextBox1.Text = exePath;
+
+                        string[] zoneFiles = Directory.GetFiles(mapsPath, "*.zone", SearchOption.TopDirectoryOnly);
+                        foreach (string zf in zoneFiles)
+                        {
+
+                            Zone zone = new FauFau.Formats.Zone();
+                            zone.Read(zf);
+                            darkListView1.Items.Add(new DarkUI.Controls.DarkListItem(zone.Name));
+                        }
+
+                        Properties.Settings.Default.Path = path;
+                        Properties.Settings.Default.Save();
+
+                        return;
+                    }
+                }
+            }
+
+
+            mapsPath = null;
+            exePath = null;
+            Properties.Settings.Default.Path = null;
+            Properties.Settings.Default.Save();
+
+            if(!ignoreError)
+            {
+                darkTextBox1.Text = "Invalid path, try again..";
+            }
+            
+        }
+
+        private void DarkButton1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void DarkListView1_DoubleClick(object sender, EventArgs e)
+        {
+            if(darkListView1.SelectedIndices.Count > 0)
+            {
+                string selected = darkListView1.Items[darkListView1.SelectedIndices[0]].Text;
+                Console.WriteLine(selected);
+
+            }
+        }
+
+        private void OpenFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            TryLoad(openFileDialog1.FileName);
+        }
+
+        private void DarkListView1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (darkListView1.SelectedIndices.Count > 0)
+                {
+                    string selected = darkListView1.Items[darkListView1.SelectedIndices[0]].Text;
+                    Console.WriteLine(selected);
+                }
+            }
+        }
+    }
+}
